84baf02759f4edcc8b284b3f1f5c70726e846493
Morteza Ansarinia <ansarinia@me.com>
2017-03-07 21:24:48 +0330

    Initial commit

---
+++ src/main/java/social/cut/Application.java
@@ -0,0 +1,12 @@
+package social.cut;
+
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+
+public class Application extends VertxNubes {
+
+  private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
+  public static void main(String[] args) {
+  }
+}
