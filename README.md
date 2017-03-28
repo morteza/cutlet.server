b8f1c8c298dc5fbb66045c1f3355ba42abbe80df
Morteza Ansarinia <ansarinia@me.com>
2017-03-28 15:41:35 +0430

    Initial commit (incl. gradle, vert.x, RESTEasy, MongoDB, lombok, and sample verticle).

---
+++ README.md
@@ -0,0 +1,3 @@
+# A cut.social Server
+
+API server using vert.x, mongodb, gradle, and love.
---
+++ src/main/java/social/cut/MainVerticle.java
@@ -0,0 +1,14 @@
+package social.cut;
+
+import io.vertx.core.AbstractVerticle;
+
+public class MainVerticle extends AbstractVerticle {
+
+  @Override
+  public void start() {
+    vertx.createHttpServer()
+        .requestHandler(req -> req.response().end("Hello cut.social!"))
+        .listen(8080);
+  }
+
+}
