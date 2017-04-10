ab7f5c259a3b4cff8f8c3db653c22d9b889dbf5e
Morteza Ansarinia <ansarinia@me.com>
2017-04-10 20:11:05 +0430

    PoC JWT/Mongo authentication.

--- src/main/java/social/cut/Server.java
+++ src/main/java/social/cut/Server.java
@@ -1,5 +1,9 @@
 package social.cut;
 
+import java.io.File;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
@@ -11,8 +15,15 @@ import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.mongo.MongoClient;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.AuthHandler;
 import io.vertx.ext.web.handler.CorsHandler;
+import io.vertx.ext.web.handler.JWTAuthHandler;
 import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.auth.AuthProvider;
+import io.vertx.ext.auth.jwt.JWT;
+import io.vertx.ext.auth.jwt.JWTAuth;
+import io.vertx.ext.auth.jwt.JWTOptions;
+import io.vertx.ext.auth.jwt.impl.JWTAuthProviderImpl;
 import social.cut.cms.CMSController;
 import social.cut.inbox.InboxController;
 import social.cut.utils.DeploymentOptionsUtils;
@@ -21,6 +32,7 @@ public class Server extends AbstractVerticle {
 
   private final Logger LOG = LoggerFactory.getLogger(Server.class);
 
+  private MongoClient mongo;
   @Override
   public void start() {
 
@@ -30,10 +42,11 @@ public class Server extends AbstractVerticle {
         
     Router router = Router.router(vertx);
 
-    MongoClient mongo = initMongo(confs.getJsonObject("mongo"));
+    mongo = initMongo(confs.getJsonObject("mongo"));
     
     addCorsHandler(router);
     addStaticHandler(router);
+    addAuthProvider(router);
 
     Injector injector = Guice.createInjector(new CutletModule(vertx, router, mongo));
 
@@ -68,5 +81,60 @@ public class Server extends AbstractVerticle {
     router.route("/assets/*").handler(StaticHandler.create());
     return router;
   }
+  
+  private Router addAuthProvider(Router router) {
+    JsonObject config = new JsonObject().put("keyStore", new JsonObject()
+        .put("path", "keystore.jceks")
+        .put("type", "jceks")
+        .put("password", "secret"));
+    
+    JWTAuth jwt = JWTAuth.create(vertx, config);
+
+    router.route("/api/*").handler(JWTAuthHandler.create(jwt, "/api/v1/auth"));
+    
+    router.get("/api/v1/me").handler(ctx -> {
+      LOG.info(ctx.user());
+      ctx.response().end(ctx.user().principal().encodePrettily());
+    });
+    
+    router.get("/api/v1/auth").handler(ctx -> {
+      String username = ctx.request().getParam("username");
+      String password = ctx.request().getParam("password");
+      
+      JsonObject query = new JsonObject()
+          .put("username", username)
+          .put("password", sha512(password, username));
+
+      mongo.findOne("social.cut.accounts", query, new JsonObject(), res -> {
+        if (res.succeeded() && res.result()!=null) {
+          String token = jwt.generateToken(new JsonObject().put("sub",res.result().getString("username")),new JWTOptions());
+          ctx.response().end(token);           
+        } else {
+          ctx.response().setStatusCode(403).end("Invalid username or password.");
+        }
+      });
+      
+    });
+    
+    return router;
+  }
+  
+  public String sha512(String password, String salt){
+    String generatedPassword = null;
+        try {
+             MessageDigest md = MessageDigest.getInstance("SHA-512");
+             md.update(salt.getBytes("UTF-8"));
+             byte[] bytes = md.digest(password.getBytes("UTF-8"));
+             StringBuilder sb = new StringBuilder();
+             for(int i=0; i< bytes.length ;i++){
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+             }
+             generatedPassword = sb.toString();
+            } 
+           catch (Exception e){
+            e.printStackTrace();
+           }
+        return generatedPassword;
+    }
 
 }
